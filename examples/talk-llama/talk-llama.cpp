1e7f10202077c622aebf28feb2c27210c18c557a
breakAIDev <tonywilson930727@gmail.com>
2025-06-20 01:19:00 +0700

    network status communication test

--- examples/talk-llama/talk-llama.cpp
+++ examples/talk-llama/talk-llama.cpp
@@ -780,22 +780,23 @@ int main(int argc, char ** argv) {
             params.person + chat_symb,
         };
 
-        // // Launch a thread to read stdin status commands
-        // std::thread stdin_thread([]() {
-        //     char line[16];
-        //     while (true) {
-        //         if (fgets(line, sizeof(line), stdin)) {
-        //             std::string status(line);
-        //             status.erase(std::remove(status.begin(), status.end(), '\n'), status.end());
-        //             if (status == "ON" || status == "OFF") {
-        //                 std::lock_guard<std::mutex> lock(g_net_mutex);
-        //                 g_net_status = status;
-        //                 printf("[stdin_thread] Network status updated: %s\n", status.c_str());
-        //             }
-        //         }
-        //     }
-        // });
-        // stdin_thread.detach();  // Run in background
+        // Launch a thread to read stdin status commands
+        std::thread stdin_thread([]() {
+            char line[16] = {0};
+            while (true) {
+                scanf("%s\n", line);
+                std::string status(line);
+                status.erase(std::remove(status.begin(), status.end(), '\n'), status.end());
+                if (status == "ON" || status == "OFF") {
+                    std::lock_guard<std::mutex> lock(g_net_mutex);
+                    g_net_status = status;
+                    printf("[stdin_thread] Network status updated: %s\n", status.c_str());
+                }
+
+                memset(line, 0, sizeof(line));
+            }
+        });
+        stdin_thread.detach();  // Run in background
 
         std::string current_status;
 
@@ -864,15 +865,15 @@ int main(int argc, char ** argv) {
 
                 printf("%s%s: %s%s\n", "\033[1m", params.person.c_str(), result.c_str(), "\033[0m");
                 
-                // {
-                //     std::lock_guard<std::mutex> lock(g_net_mutex);
-                //     current_status = g_net_status;
-                // }
+                {
+                    std::lock_guard<std::mutex> lock(g_net_mutex);
+                    current_status = g_net_status;
+                }
 
-                // if (current_status == "ON") {
-                //     audio.clear();
-                //     continue;
-                // }
+                if (current_status == "ON") {
+                    audio.clear();
+                    continue;
+                }
                 
                 const std::vector<llama_token> tokens = llama_tokenize(ctx_llama, result.c_str(), false);
 
